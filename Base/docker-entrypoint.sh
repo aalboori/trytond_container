5bcf7f969f8167eae7b3c8474292e3c9c563797c
Ahmed AlBoori <ahmed.boori@gmail.com>
2016-09-10 05:22:17 +0300

    add sao client

--- Base/docker-entrypoint.sh
+++ Base/docker-entrypoint.sh
@@ -3,7 +3,17 @@ set -e
 
 if [ "$1" = 'trytond' ]; then
 	
-	exec gosu /usr/local/bin/trytond -c trytond.conf -v
+	# Run setup scripts for the server
+	if [ -d /docker-entrypoint-init.d ]; then
+		for f in /docker-entrypoint-init.d/*.sh; do
+			[ -f "$f" ] && . "$f"
+		done
+	fi
+	
+	# Reset permissions to the data directory
+	chown -R trytond:trytond "$TRYTOND_DATA"
+	
+	exec gosu trytond /usr/local/bin/trytond -c trytond.conf -v
 fi
 
 exec "$@"
